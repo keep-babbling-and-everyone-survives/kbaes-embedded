f1b1e9e8b52cfa426d1ae19daa051a690a2e6e2b
QuentinBouvier <32564163+QuentinBouvier@users.noreply.github.com>
2019-01-08 17:02:03 +0100

    Used options to externalized private infos from repo

--- src/kbaes.py
+++ src/kbaes.py
@@ -2,13 +2,19 @@ import tornado.ioloop
 import tornado.gen
 import tornadis
 
+from tornado.options import options, define
 from routines.ApiMessageHandler import ApiMessageHandler as ApiMessageHandler
 
-channel = "private-board.1"
+define('redis_address')
+define('redis_port', type=int)
+define('channel_name')
+define('board_id', type=int)
 
-client = tornadis.PubSubClient(host="192.168.33.10", port=6379, autoconnect=False)
+options.parse_config_file('./kbaes.conf')
 
-listener = ApiMessageHandler(client, channel)
+client = tornadis.PubSubClient(host=options.redis_address, port=options.redis_port, autoconnect=False)
+
+listener = ApiMessageHandler(client, "%s%d" % (options.channel_name, options.board_id))
 
 if __name__ == '__main__':
     tornado.ioloop.IOLoop.instance().run_sync(listener.startListening)
