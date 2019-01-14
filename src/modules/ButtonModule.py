445b81f922eed4739efd8ba43cb28835ac8fcffe
ValentinArnould <valentin.arnould@outlook.com>
2019-01-14 17:25:19 +0100

    adaptation of ButtonModule for the board

--- src/modules/ButtonModule.py
+++ src/modules/ButtonModule.py
@@ -1,22 +1,9 @@
 import RPi.GPIO as GPIO #librairies pour les broches de la Rpi & du temps
 import time
+import json
+import tornado.gen
 
-GPIO.setmode(GPIO.BOARD) #mode broches board (independant du modele de la carte)
-GPIO.setwarnings(False) #efface les avertissements
-buttons = [12,16,22] #broches des boutons
-leds = [32,36,38,40] #broches des leds
-
-for led in leds: #borches des leds en sortie en LOW
- GPIO.setup(led,GPIO.OUT)
- GPIO.output(led,GPIO.LOW)
-
-for button in buttons: #broches des boutons en entree
- GPIO.setup(button,GPIO.IN)
-
-serie_l = [9,3,4,10,9,7,11,10,1,15] #serie de leds du jeu a enlever apres connexion
-serie_b = [2,1,2,4,5,1,7,2,1,6] #serie de boutons du jeu a enlever apres connexion
-
-def switch_leds(data): #effectue le changement des leds par bit shifting
+def switch_leds(data, leds): #effectue le changement des leds par bit shifting
  c = 0
  while data > 0 and c < len(leds):
   if data & 1:
@@ -27,7 +14,7 @@ def switch_leds(data): #effectue le changement des leds par bit shifting
   data = data >> 1
 
 
-def b_trigger(): #controle si au moins 1 bouton est enfonce
+def b_trigger(buttons): #controle si au moins 1 bouton est enfonce
  n = 0
  if GPIO.input(buttons[0]):
   n = n + 1
@@ -38,19 +25,32 @@ def b_trigger(): #controle si au moins 1 bouton est enfonce
  #print("boutons {}".format(n))
  return n
 
-def l_and_b(leds):
- switch_leds(leds) #change les leds
+@tornado.gen.coroutine
+def playModule(ruleset):
+ GPIO.setmode(GPIO.BOARD)
+ GPIO.setwarnings(False)
+ buttons = [12,16,22]
+ leds = [32,36,38,40] #broches des leds
+
+ for led in leds: #borches des leds en sortie en LOW
+  GPIO.setup(led,GPIO.OUT)
+  GPIO.output(led,GPIO.LOW)
+
+ for button in buttons: #broches des boutons en entree
+  GPIO.setup(button,GPIO.IN)
+
+ switch_leds(ruleset.combination, leds) #change les leds
  b_a=b_b=b_c= 0 #met les var a 0
  b_send=sequence = 0
  while b_send == 0: #tant que la combinaison actuelle n'est pas envoyee
   b_a=b_b=b_c= 0
   sequence = 0 #redemarre la sequence de boutons
   time.sleep(0.1) #definit le temps qui passe
-  trigger = b_trigger() #verifie si un bouton est enfonce
+  trigger = b_trigger(buttons) #verifie si un bouton est enfonce
   #print trigger
   while trigger > 0: #si en bouton est enfonce
    #print "yolo"
-   trigger = b_trigger() #verifie a nouveau
+   trigger = b_trigger(buttons) #verifie a nouveau
    if GPIO.input(buttons[0]): #pour chaque bouton definit une variable correspondante
     b_a = 1
     #print (GPIO.input(buttons[0]))
@@ -74,8 +74,19 @@ def l_and_b(leds):
    #sequence = 0
   else:
    sequence = 0
- return sequence
+ 
+ answer = yield tornado.gen.maybe_future(convertToJson(sequence, ruleset))
+ raise tornado.gen.Return(answer)
 
 #while True:
 # butt = l_and_b(0)
 # print butt
+
+def convertToJson(answer, ruleset):
+ modules = []
+ moduleslen = len(ruleset.modules)
+ for n in range(moduleslen):
+  bit = 2**(moduleslen-(n+1))
+  modules.append({"name": ruleset.modules[n].name, "solution": answer & bit > 0})
+ return modules
+
